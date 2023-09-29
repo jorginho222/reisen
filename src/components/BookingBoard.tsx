4f805bcd89c7cca7223cb0a3584a8c8cabd62af8
jorginho222 <rodizioperalta22@gmail.com>
2023-09-29 15:36:44 -0300

    Finish validation and editing

--- src/components/BookingBoard.tsx
+++ src/components/BookingBoard.tsx
@@ -4,7 +4,7 @@ import {BookingRequest} from "../interfaces/BookingRequest.ts";
 import {v4 as uuidV4} from "uuid";
 import {HousingTypes} from "../types/HousingTypes.ts";
 import {PaymentOptions} from "../types/PaymentOptions.ts";
-import {useEffect, useMemo, useState} from "react";
+import {useMemo, useState} from "react";
 import {useBooking} from "../store.tsx";
 import {BookingForm} from "./BookingForm.tsx";
 import {useForm} from "react-hook-form";
@@ -24,11 +24,21 @@ export function BookingBoard() {
     totalAmount: 0,
     signedAmount: 0
   }
-  const [booking, setBooking] = useState<BookingRequest>(cleanBooking)
   const [index, setIndex] = useState(-1)
 
-  const  { register, handleSubmit, setValue, setError, formState: errors } = useForm({
-    resolver: yupResolver()
+  const validationSchema = Yup.object().shape({
+    name: Yup.string().required('El nombre es requerido'),
+    place: Yup.string().required('El lugar es requerido'),
+    origin: Yup.string().required('El medio de reserva es requerido'),
+    // checkIn: Yup.date().required('La fecha de Check In es requerida'),
+    // checkOut: Yup.date().required('La fecha de Check Out es requerida'),
+    housingType: Yup.string().required('El tipo de alojamiento es requerido'),
+    paymentOption: Yup.string().required('El tipo de pago es requerido'),
+    totalAmount: Yup.number().required('El monto total es requerido'),
+  })
+
+  const  { register, control, handleSubmit, setValue, reset, formState: {errors} } = useForm<BookingRequest|any>({
+    resolver: yupResolver(validationSchema)
   })
 
   const bookingList = useBooking(state => state.bookings)
@@ -57,21 +67,29 @@ export function BookingBoard() {
 
   const getTotal = useMemo(() => getPaidTotal + getNoPaidTotal, [getPaidTotal, getNoPaidTotal])
 
-  const createBooking = data => {
-    console.log(data)
-    // index.valueOf() > -1
-    //   ? updateBooking(booking, index)
-    //   : addBooking(booking)
-    //
-    // setBooking(cleanBooking)
-    // setIndex(-1)
+  const createBooking = (booking: BookingRequest) => {
+    console.log(booking)
+    index.valueOf() > -1
+      ? updateBooking(booking, index)
+      : addBooking(booking)
+
+    reset()
+    setIndex(-1)
   }
   const addBooking = useBooking(state => state.addBooking)
   const updateBooking = useBooking(state => state.updateBooking)
 
   const editBooking = (booking: BookingRequest) => {
     setIndex(bookingList.indexOf(booking))
-    setBooking(booking)
+    setValue('name', booking.name)
+    setValue('place', booking.place)
+    setValue('origin', booking.origin)
+    // setValue('checkIn', booking.checkIn)
+    // setValue('checkOut', booking.checkOut)
+    setValue('housingType', booking.housingType)
+    setValue('paymentOption', booking.paymentOption)
+    setValue('totalAmount', booking.totalAmount)
+    setValue('signedAmount', booking.signedAmount)
   }
   const deleteBooking = (booking: BookingRequest) => {
     setIndex(bookingList.indexOf(booking))
@@ -91,9 +109,9 @@ export function BookingBoard() {
         <Box sx={{mt: 2}}>
           <ul>
             {bookingList.map(booking =>
-              (<li key={booking.id}>
+              (<li>
                 <Grid container spacing={2}>
-                  <Grid item xs={6}>
+                  <Grid item xs={6} key={booking.id}>
                     {booking.checkIn.toLocaleDateString('es-AR')} al {booking.checkOut.toLocaleDateString('es-AR')}: {booking.name}
                   </Grid>
                   <Grid item xs={2}>
@@ -128,20 +146,23 @@ export function BookingBoard() {
           </Grid>
         }
       </Card>
-      <form onSubmit={handleSubmit(createBooking)}>
-        <BookingForm
-          register={register}
-        />
-        <Box sx={{my: 2}}>
-          <Button
-            type="submit"
-            color="success"
-            variant="outlined"
-          >
-            Guardar
-          </Button>
-        </Box>
-      </form>
+      <Card  sx={{mt: 2}}>
+        <form onSubmit={handleSubmit(createBooking)}>
+          <BookingForm
+            register={register}
+            errors={errors}
+          />
+          <Box sx={{my: 2}}>
+            <Button
+              type="submit"
+              color="success"
+              variant="outlined"
+            >
+              Guardar
+            </Button>
+          </Box>
+        </form>
+      </Card>
     </>
   )
 }
--- src/components/BookingForm.tsx
+++ src/components/BookingForm.tsx
@@ -1,29 +1,25 @@
 import {
   Box,
-  Button, Card,
   FormControl,
   InputAdornment,
   InputLabel,
   MenuItem,
   OutlinedInput,
   Select,
-  TextField
+  TextField, Typography
 } from "@mui/material";
 import {DateTimePicker, LocalizationProvider} from "@mui/x-date-pickers";
 import {AdapterDayjs} from "@mui/x-date-pickers/AdapterDayjs";
-import dayjs, {name} from "dayjs";
 import {PaymentOptions} from "../types/PaymentOptions.ts";
 import {HousingTypes} from "../types/HousingTypes.ts";
-import {useBooking} from "../store.tsx";
-import {BookingRequest} from "../interfaces/BookingRequest.ts";
-import {UseFormRegister, UseFormRegisterReturn} from "react-hook-form/dist/types/form";
-import {FieldPath} from "react-hook-form";
+import {useState} from "react";
 
 interface BookingFormProps {
-  register
+  register,
+  errors,
 }
 
-export function BookingForm({ register }: BookingFormProps) {
+export function BookingForm({ register, errors }: BookingFormProps) {
 
   const paymentOptions = [
     {text: 'Está Pago', value: PaymentOptions.Paid},
@@ -35,120 +31,128 @@ export function BookingForm({ register }: BookingFormProps) {
     {text: 'Habitación', value: HousingTypes.Room},
     {text: 'Hostel', value: HousingTypes.Hostel}
   ]
+  const [selectedPaymentOption, setSelectedPaymentOption] = useState<PaymentOptions>()
 
   return (
     <>
-      <Card  sx={{mt: 2}}>
-        <Box
-          sx={{
-            '& .MuiTextField-root': { m: 1, width: '50ch', mt: 2 },
-          }}
-        >
-          <TextField
-            id="outlined-multiline-flexible"
-            placeholder="Nombre Lugar"
-            // onChange={(evt) => {
-            //   setBooking({...booking, name: evt.target.value})
-            // }}
-            {...register('name')}
-          />
-          <TextField
-            id="outlined-multiline-flexible-2"
-            placeholder="¿Donde queda?"
-            // onChange={(evt) => {
-            //   setBooking({...booking, place: evt.target.value})
-            // }}
-            {...register('place')}
-          />
-          <TextField
-            id="outlined-multiline-flexible-2"
-            placeholder="Medio de Reserva"
-            // value={booking.origin}
-            // onChange={(evt) => {
-            //   setBooking({...booking, origin: evt.target.value})
-            // }}
-            {...register('origin')}
-          />
-          <div>
-            <LocalizationProvider dateAdapter={AdapterDayjs}>
-              <DateTimePicker
-                // value={dayjs(booking.checkIn)}
-                label="Check In"
-                // onChange={(evt) => {
-                //   setBooking({...booking, checkIn: evt.$d})
-                // }}
-                {...register('checkIn', { valueAsDate: true})}
-              />
-              <DateTimePicker
-                // value={dayjs(booking.checkOut)}
-                label="Check Out"
-                // onChange={(evt) => setBooking({...booking, checkOut: evt.$d})}
-                {...register('checkOut', { valueAsDate: true})}
-              />
-            </LocalizationProvider>
-          </div>
-          <FormControl sx={{ m: 1, minWidth: 300 }}>
-            <InputLabel id="demo-simple-select-label">Tipo Alojamiento</InputLabel>
-            <Select
-              labelId="demo-simple-select-label"
-              id="demo-simple-select"
-              // value={booking.housingType}
-              // onChange={(e) => setBooking({...booking, housingType: e.target.value})}
-              label="Tipo Alojamiento"
-              {...register('housingType')}
-            >
-              {housingTypes.map(type => (
-                  <MenuItem key={type.value} value={type.value}>{type.text}</MenuItem>
-                )
-              )}
-            </Select>
-          </FormControl>
-          <FormControl sx={{m: 1, minWidth: 300}}>
-            <InputLabel id="demo-simple-select-label">Opción de Pago</InputLabel>
-            <Select
-              labelId="demo-simple-select-label"
-              id="demo-simple-select"
-              // value={booking.paymentOption}
-              // onChange={e => setBooking({...booking, paymentOption: e.target.value})}
-              label="Opción de Pago"
-              {...register('paymentOption')}
-            >
-              {paymentOptions.map(option => (
-                  <MenuItem key={option.value} value={option.value}>{option.text}</MenuItem>
-                )
-              )}
-            </Select>
-          </FormControl>
-          <div>
-            {/*{booking.paymentOption === PaymentOptions.Signed && (*/}
-            {/*  <FormControl sx={{ m: 1 }}>*/}
-            {/*    <InputLabel id="demo-simple-select-label">Seña</InputLabel>*/}
-            {/*    <OutlinedInput*/}
-            {/*      id="outlined-adornment-amount"*/}
-            {/*      // value={booking.signedAmount}*/}
-            {/*      type="number"*/}
-            {/*      startAdornment={<InputAdornment position="start">$</InputAdornment>}*/}
-            {/*      label="Seña"*/}
-            {/*      // onChange={(e) => setBooking({...booking, signedAmount: Number(e.target.value)})}*/}
-            {/*      {...register('signedAmount')}*/}
-            {/*    />*/}
-            {/*  </FormControl>*/}
-            {/*)}*/}
+      <Box
+        sx={{
+          '& .MuiTextField-root': { m: 1, width: '80ch', mt: 2 },
+        }}
+      >
+        <TextField
+          id="outlined-multiline-flexible"
+          placeholder="Nombre Lugar"
+          {...register('name')}
+          error={!!errors.name}
+        />
+        <Typography variant="inherit" color="textSecondary">
+          {errors.name?.message}
+        </Typography>
+        <TextField
+          id="outlined-multiline-flexible-2"
+          placeholder="¿Donde queda?"
+          {...register('place')}
+          error={!!errors.place}
+        />
+        <Typography variant="inherit" color="textSecondary">
+          {errors.place?.message}
+        </Typography>
+        <TextField
+          id="outlined-multiline-flexible-2"
+          placeholder="Medio de Reserva"
+          {...register('origin')}
+          error={!!errors.origin}
+        />
+        <Typography variant="inherit" color="textSecondary">
+          {errors.origin?.message}
+        </Typography>
+        <div>
+          <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <DateTimePicker
+              defaultValue=""
+              // value={dayjs(booking.checkIn)}
+              label="Check In"
+              {...register('checkIn', { valueAsDate: true })}
+            />
+            <DateTimePicker
+              defaultValue=""
+              // value={dayjs(booking.checkOut)}
+              label="Check Out"
+              {...register('checkOut', { valueAsDate: true })}
+            />
+          </LocalizationProvider>
+        </div>
+        <FormControl sx={{ m: 1, minWidth: 300 }}>
+          <InputLabel id="demo-simple-select-label">Tipo Alojamiento</InputLabel>
+          <Select
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            defaultValue=""
+            label="Tipo Alojamiento"
+            {...register('housingType')}
+            error={!!errors.housingType}
+          >
+            {housingTypes.map(type => (
+                <MenuItem key={type.value} value={type.value}>{type.text}</MenuItem>
+              )
+            )}
+          </Select>
+          <Typography variant="inherit" color="textSecondary">
+            {errors.housingType?.message}
+          </Typography>
+        </FormControl>
+        <FormControl sx={{m: 1, minWidth: 300}}>
+          <InputLabel id="demo-simple-select-label">Opción de Pago</InputLabel>
+          <Select
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            label="Opción de Pago"
+            defaultValue=""
+            {...register('paymentOption')}
+            onChange={(event) => {
+              // @ts-ignore
+              setSelectedPaymentOption(event.target.value)
+            }}
+            error={!!errors.paymentOption}
+          >
+            {paymentOptions.map(option => (
+                <MenuItem key={option.value} value={option.value}>{option.text}</MenuItem>
+              )
+            )}
+          </Select>
+          <Typography variant="inherit" color="textSecondary">
+            {errors.paymentOption?.message}
+          </Typography>
+        </FormControl>
+        <div>
+          {selectedPaymentOption === PaymentOptions.Signed && (
             <FormControl sx={{ m: 1 }}>
-              {/*<InputLabel id="demo-simple-select-label">{booking.paymentOption === PaymentOptions.NoPaid ? 'Monto Aprox.' : 'Monto Total'}</InputLabel>*/}
+              <InputLabel id="demo-simple-select-label">Seña</InputLabel>
               <OutlinedInput
                 id="outlined-adornment-amount"
-                // value={booking.totalAmount}
                 type="number"
                 startAdornment={<InputAdornment position="start">$</InputAdornment>}
-                // label={booking.paymentOption === PaymentOptions.NoPaid ? 'Monto Aprox.' : 'Monto Total'}
-                // onChange={(e) => setBooking({...booking, totalAmount: Number(e.target.value)})}
-                {...register('totalAmount')}
+                label="Seña"
+                {...register('signedAmount')}
               />
             </FormControl>
-          </div>
-        </Box>
-      </Card>
+          )}
+          <FormControl sx={{ m: 1 }}>
+            <InputLabel id="demo-simple-select-label">{selectedPaymentOption === PaymentOptions.NoPaid ? 'Monto Aprox.' : 'Monto Total'}</InputLabel>
+            <OutlinedInput
+              id="outlined-adornment-amount"
+              type="number"
+              startAdornment={<InputAdornment position="start">$</InputAdornment>}
+              {...register('totalAmount')}
+              error={!!errors.totalAmount}
+            />
+            <Typography variant="inherit" color="textSecondary">
+              {errors.totalAmount?.message}
+            </Typography>
+          </FormControl>
+        </div>
+      </Box>
     </>
   );
 };
